f71ba060a997e6eec46f7167c2eec6bd4d4ced72
nweke joseph <nwekejoseph52@gmail.com>
2023-03-02 08:35:38 +0100

    added function to faq component

--- src/app/component/faq/faq.component.ts
+++ src/app/component/faq/faq.component.ts
@@ -12,26 +12,37 @@ export class FaqComponent {
       question: 'What is Bookmark?',
       answer:
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui et aliquamtenetur laudantium quae possimus magnam natus, temporibus voluptatemaspernatur explicabo quia dicta, at amet expedita quasi unde.',
-      faqOpen: false,
+      faqOpened: false,
     },
     {
       question: 'How can I request a new browser?',
       answer:
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui et aliquamtenetur laudantium quae possimus magnam natus, temporibus voluptatemaspernatur explicabo quia dicta, at amet expedita quasi unde.',
-      faqOpen: false,
+      faqOpened: false,
     },
     {
       question: 'Is there a mobile app?',
       answer:
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui et aliquamtenetur laudantium quae possimus magnam natus, temporibus voluptatemaspernatur explicabo quia dicta, at amet expedita quasi unde.',
-      faqOpen: false,
+      faqOpened: false,
     },
     {
       question: 'What about other Chromium browsers?',
       answer:
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui et aliquamtenetur laudantium quae possimus magnam natus, temporibus voluptatemaspernatur explicabo quia dicta, at amet expedita quasi unde.',
-      faqOpen: false,
+      faqOpened: false,
     },
   ];
-  toggleFaq(faq: iFaq) {}
+
+  closeOpenedFaq(faq: iFaq) {
+    const openedFaq = this.faqs.filter(
+      (obj: iFaq) => obj !== faq && obj.faqOpened === true
+    );
+
+    if (openedFaq[0]) openedFaq[0].faqOpened = false;
+  }
+  toggleFaq(faq: iFaq) {
+    this.closeOpenedFaq(faq);
+    faq.faqOpened = !faq.faqOpened;
+  }
 }
