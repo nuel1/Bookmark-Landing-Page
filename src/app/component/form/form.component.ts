c9a2672447408eedcd0501284db3026a9b68c712
nweke joseph <nwekejoseph52@gmail.com>
2023-03-02 08:33:58 +0100

    created form component

---
+++ src/app/component/form/form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent {
+  emailPattern: RegExp = /^\w+@[A-Za-z0-9.]+?\.[A-Za-z]+$/;
+  error = false;
+  validateEmail(email: string) {
+    this.emailPattern.test(email) ? (this.error = false) : (this.error = true);
+  }
+}
