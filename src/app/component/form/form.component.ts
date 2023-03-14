430dbafe866ba0ba6bfa35f04fb00dbdd34012c7
nweke joseph <nwekejoseph52@gmail.com>
2023-03-14 21:51:30 +0100

    fixed all bugs

--- src/app/component/form/form.component.ts
+++ src/app/component/form/form.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
-  emailPattern: RegExp = /^\w+@[A-Za-z0-9.]+?\.[A-Za-z]+$/;
+  emailPattern: RegExp = /^\w+@[A-Za-z0-9]+?\.[A-Za-z]+$/;
   error = false;
   validateEmail(email: string) {
     this.emailPattern.test(email) ? (this.error = false) : (this.error = true);
